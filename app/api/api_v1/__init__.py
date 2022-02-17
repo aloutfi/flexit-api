0df599802157beab73575726ea5aa20e29956c9e
Andrew Loutfi <loofinator@gmail.com>
2022-02-16 19:54:44 -0600

    feature: 1.0.0

--- app/api/api_v1/__init__.py
+++ app/api/api_v1/__init__.py
@@ -3,9 +3,11 @@ from fastapi import Depends, APIRouter
 from . import flexit_auth, flexit_queries
 
 api_router = APIRouter()
-api_router.include_router(flexit_auth.router)
+
 api_router.include_router(
     flexit_queries.router,
     prefix="/queries",
     dependencies=[Depends(flexit_auth.get_current_active_user)],
 )
+
+api_router.include_router(flexit_auth.router)
--- app/main.py
+++ app/main.py
@@ -2,10 +2,10 @@ from fastapi import FastAPI
 
 from app.api.api_v1 import api_router
 
-app = FastAPI(docs_url="/", redoc_url=None)
+app = FastAPI(title="Flexit-API", docs_url="/", redoc_url=None, version="1.0.0")
 app.include_router(api_router)
 
 
-@app.get("/health_check")
+@app.get("/health_check", tags=["utilities"])
 async def health_check():
     return {"message": "Looks like the app is healthy"}
